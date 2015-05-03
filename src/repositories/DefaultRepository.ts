646619a01b32caf1dbb54fb612f9f44856b2917c
wernerdegroot <github@wernerdegroot.nl>
2015-05-03 19:13:41 +0200

    Convert JSON to subject using a Template

--- src/repositories/DefaultRepository.ts
+++ src/repositories/DefaultRepository.ts
@@ -1,7 +1,9 @@
+/// <reference path="../../src/dependencies.ts" />
 /// <reference path="../../src/repositories/Repository.ts" />
 /// <reference path="../../src/dependencies.ts" />
 /// <reference path="../../src/subjects/IdSubject.ts" />
 /// <reference path="../../src/contexts/Context.ts" />
+/// <reference path="../../src/templates/Template.ts" />
 
 module repositories {
 	
@@ -9,6 +11,8 @@ module repositories {
 	import IdSubject = subjects.IdSubject;
 	import IHttpService = angular.IHttpService;
 	import Context = contexts.Context;
+	import Template = templates.Template;
+	import IHttpPromiseCallbackArg = angular.IHttpPromiseCallbackArg
 	
 	export class DefaultRepository<ID_TYPE, SUBJECT_TYPE extends IdSubject<any>> /*implements Repository<ID_TYPE, SUBJECT_TYPE>*/ {
 		
@@ -16,10 +20,11 @@ module repositories {
 			
 		}
 		
-		getById(id: ID_TYPE, context: Context<ID_TYPE>): IPromise<SUBJECT_TYPE> {
-			return this.$http.get<SUBJECT_TYPE>(context.getSingleUrl(id));	
+		getById(id: ID_TYPE, context: Context<ID_TYPE>, template: Template<SUBJECT_TYPE>): IPromise<SUBJECT_TYPE> {
+			return this.$http.get<Object>(context.getSingleUrl(id)).then((response: IHttpPromiseCallbackArg<Object>) => {
+				return template.fromJson(response.data);
+			});	 
 		}
-		
 	}
 	
 }
--- test/repositories/DefaultRepositoryTest.ts
+++ test/repositories/DefaultRepositoryTest.ts
@@ -5,21 +5,29 @@ module repositories {
 	
 	import IHttpService = angular.IHttpService;
 	import IHttpBackendService = angular.IHttpBackendService;
+	import IQService = angular.IQService;
 	
 	describe('DefaultRepository',  () => {
 		
 		var http: IHttpService;
 		var httpBackend: IHttpBackendService;
+		var q: IQService;
 		
-		beforeEach(inject(($httpBackend: IHttpBackendService, $http: IHttpService) => {
+		beforeEach(inject(($httpBackend: IHttpBackendService, $http: IHttpService, $q: IQService) => {
 			http = $http;
 			httpBackend = $httpBackend;
+			q = $q;
 		}));
 		
 		it('should return a promise to a JSON-object when getById is called', () => {
 			
 			var id: number = 42;
 			var url: string = '/api/some-resource/42';
+			
+			// Mock a subject.
+			var expectedSubject: Object = {
+				guid: '0c8b736a1604'
+			};
 			
 			// Mock a Context.
 			var context = {
@@ -28,9 +36,15 @@ module repositories {
             };
 			context.getSingleUrl.withArgs(id).returns(url);
 			
+			// Mock a Template.
+			var template = {
+				fromJson: sinon.stub()	
+			};
+			template.fromJson.returns(q.when(expectedSubject));
+			
 			// Mock a resource.
 			var responseData = {
-				payload: 'payload'	
+				guid: '7bf39e45713b'	
 			};
 			httpBackend.when('GET', url).respond(responseData);
 			
@@ -39,12 +53,12 @@ module repositories {
 			
 			// Obtain a response through the DefaultRepository.
 			// Make sure that the response matches our expectations.
-			var responsePromise = defaultRepository.getById(id, context);
-			responsePromise.then((response) => {
-				expect(response.data).to.eql(responseData);
+			var responsePromise = defaultRepository.getById(id, context, <any> template);
+			responsePromise.then((subject: Object) => {
+				expect(subject).to.equal(expectedSubject);
 			});
 			
-			// Flush to resolve the promise and execute the expectations.
+			// Flush to resolve the promises and execute the expectations.
 			httpBackend.flush();
 			
 		});
