8618475f37cea57c302ad84a775fbe16c37a3882
wernerdegroot <github@wernerdegroot.nl>
2015-05-04 19:47:39 +0200

    Give DefaultRepository a way to obtain all subjects

--- src/repositories/DefaultRepository.ts
+++ src/repositories/DefaultRepository.ts
@@ -8,22 +8,41 @@
 module repositories {
 	
 	import IPromise = angular.IPromise;
+	import IHttpPromise = angular.IHttpPromise;
 	import IdSubject = subjects.IdSubject;
 	import IHttpService = angular.IHttpService;
 	import Context = contexts.Context;
 	import Template = templates.Template;
-	import IHttpPromiseCallbackArg = angular.IHttpPromiseCallbackArg
+	import IHttpPromiseCallbackArg = angular.IHttpPromiseCallbackArg;
+	import IQService = angular.IQService;
 	
 	export class DefaultRepository<ID_TYPE, SUBJECT_TYPE extends IdSubject<any>> /*implements Repository<ID_TYPE, SUBJECT_TYPE>*/ {
 		
-		constructor(private $http: IHttpService) {
+		constructor(private $http: IHttpService, private $q: IQService) {
 			
 		}
 		
 		getById(id: ID_TYPE, context: Context<ID_TYPE>, template: Template<SUBJECT_TYPE>): IPromise<SUBJECT_TYPE> {
-			return this.$http.get<Object>(context.getSingleUrl(id)).then((response: IHttpPromiseCallbackArg<Object>) => {
+			var responsePromise: IHttpPromise<Object> = this.$http.get<Object>(context.getSingleUrl(id));
+			var responseHandler = (response: IHttpPromiseCallbackArg<Object>) => {
 				return template.fromJson(response.data);
-			});	 
+			};
+			  
+			return responsePromise.then(responseHandler);
+		}
+		
+		getAll(context: Context<ID_TYPE>, template: Template<SUBJECT_TYPE>): IPromise<SUBJECT_TYPE[]> {
+			
+			var self = this;
+			
+			var responsePromise = this.$http.get<Object[]>(context.getAllUrl());
+			var responseHandler = (response: IHttpPromiseCallbackArg<Object[]>) => {
+				var jsonPromises: IPromise<SUBJECT_TYPE>[] 
+					= response.data.map((json: Object) => template.fromJson(json)); 
+				return self.$q.all(jsonPromises);
+			};
+			
+			return responsePromise.then(responseHandler);
 		}
 	}
 	
--- test/repositories/DefaultRepositoryTest.ts
+++ test/repositories/DefaultRepositoryTest.ts
@@ -3,72 +3,122 @@
 
 module repositories {
 	
-	import IHttpService = angular.IHttpService;
-	import IHttpBackendService = angular.IHttpBackendService;
 	import IQService = angular.IQService;
 	import IPromise = angular.IPromise;
+	import IRootScopeService = angular.IRootScopeService;
 	
 	describe('DefaultRepository',  () => {
 		
-		var http: IHttpService;
-		var httpBackend: IHttpBackendService;
 		var q: IQService;
+		var rootScope: IRootScopeService;
+		var http;
+		var someSubject: Object;
+		var anotherSubject: Object;
+		var someSubjectPromise: IPromise<Object>;
+		var anotherSubjectPromise: IPromise<Object>;
+		var someJson: Object;
+		var anotherJson: Object;
+		var someJsonResponsePromise: IPromise<Object>;
+		var allJsonResponsePromise: IPromise<Object>;
+		var singleUrl: string;
+		var allUrl: string;
+		var context;
+		var id: number = 42;
 		
-		beforeEach(inject(($httpBackend: IHttpBackendService, $http: IHttpService, $q: IQService) => {
-			http = $http;
-			httpBackend = $httpBackend;
+		beforeEach(inject(($q: IQService, $rootScope: IRootScopeService) => {
 			q = $q;
-		}));
-		
-		it('should return a promise to a JSON-object when getById is called', () => {
+			rootScope = $rootScope;
 			
-			var id: number = 42;
-			var url: string = '/api/some-resource/42';
-			
-			// Mock a subject.
-			var expectedSubject: Object = {
+			// Mock subjects.
+			someSubject = {
 				guid: '0c8b736a1604'
 			};
-			var expectedSubjectPromise: IPromise<Object> = q.when(expectedSubject);
+			someSubjectPromise = q.when(someSubject);
+			
+			anotherSubject = {
+				guid: '20442b443f9d'
+			};
+			anotherSubjectPromise = q.when(anotherSubject);
+			
+			// Mock JSON-objects.
+			someJson = {
+				guid: '7bf39e45713b'	
+			};
+			someJsonResponsePromise = q.when({data: someJson});
+			
+			anotherJson = {
+				guid: 'f9f5b3799488'
+			}
+			allJsonResponsePromise = q.when({data: [someJson, anotherJson]});
+			
+			// Some URL's.
+			singleUrl = '/api/some-resource/42';
+			allUrl = '/api/some-resource';
+			
+			// Mock $http-service.
+			http = {
+				get: sinon.stub()	
+			};
+			http.get.withArgs(singleUrl).returns(someJsonResponsePromise);
+			http.get.withArgs(allUrl).returns(allJsonResponsePromise);
 			
 			// Mock a Context.
-			var context = {
+			context = {
                 getSingleUrl: sinon.stub(),
 				getAllUrl: sinon.stub()
             };
-			context.getSingleUrl.withArgs(id).returns(url);
+			context.getSingleUrl.withArgs(id).returns(singleUrl);
+			context.getAllUrl.returns(allUrl);
+		}));
+		
+		afterEach(() => {
+            rootScope.$digest();
+        });
+		
+		it('should return a promise to a JSON-object when getById is called', () => {
+			
+			var someSubjectPromise: IPromise<Object> = q.when(someSubject);
 			
 			// Mock a Template.
 			var template = {
 				fromJson: sinon.stub()	
 			};
-			template.fromJson.returns(expectedSubjectPromise);
-			
-			// Mock a resource.
-			var responseData = {
-				guid: '7bf39e45713b'	
-			};
-			httpBackend.when('GET', url).respond(responseData);
+			template.fromJson.withArgs(someJson).returns(someSubjectPromise);
 			
 			// Construct a DefaultRepository.
-			var defaultRepository = new DefaultRepository<number, any>(http);
+			var defaultRepository = new DefaultRepository<number, any>(http, q);
 			
 			// Obtain a response through the DefaultRepository.
 			// Make sure that the response matches our expectations.
 			var responsePromise = defaultRepository.getById(id, context, <any> template);
 			responsePromise.then((subject: Object) => {
-				expect(subject).to.equal(expectedSubject);
-				
-				// The method fromJson of Template should be called with responseData.
-				expect(template.fromJson.called).to.be.true;
-				expect(template.fromJson.firstCall.args[0]).to.eql(responseData);
+				expect(subject).to.equal(someSubject);
 			});
-			
-			// Flush to resolve the promises and execute the expectations.
-			httpBackend.flush();
-			
 		});
 		
+		it('should return a promise to a JSON-object when getAll is called', () => {
+			
+			var someSubjectPromise: IPromise<Object> = q.when(someSubject);
+			
+			// Mock a Template.
+			var template = {
+				fromJson: sinon.stub()	
+			};
+			template.fromJson.withArgs(someJson).returns(someSubjectPromise);
+			template.fromJson.withArgs(anotherJson).returns(anotherSubjectPromise);
+			
+			// Construct a DefaultRepository.
+			var defaultRepository = new DefaultRepository<number, any>(http, q);
+			
+			// Obtain a response through the DefaultRepository.
+			// Make sure that the response matches our expectations.
+			var responsePromise = defaultRepository.getAll(context, <any> template);
+			responsePromise.then((subject: Object[]) => {
+				expect(subject[0]).to.equal(someSubject);
+				expect(subject[1]).to.equal(anotherSubject);
+			});
+		});
+				
 	});
 	
 }
